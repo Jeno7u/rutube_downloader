4229fb2f22d7f177a3281ca59895b76d5b6e7f99
Jeno7u <114745021+Jeno7u@users.noreply.github.com>
2022-10-03 01:00:41 +0300

    added pages
    added about page and main

--- downloader/main/views.py
+++ downloader/main/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<img src="https://i.playground.ru/p/1Fcw47v1SBe3zmpSd1ovvg.jpeg">')
+    return render(request, 'main/index.html')
 
 
 def about(request):
-    return HttpResponse('https://i.mycdn.me/getVideoPreview?id=1472155421333&idx=5&type=39&tkn=PX300WXAQP4LLMykZzAoe7_yN_U&fn=vid_l')
+    return render(request, 'main/about.html')
